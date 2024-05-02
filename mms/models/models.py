5832ef20f4c6866a728d7f1f6772245b13121b61
Riccino <riccardorinojosam@gmail.com>
2024-05-02 12:16:47 -0300

    Added comments in models.py.

--- mms/models/models.py
+++ mms/models/models.py
@@ -1,28 +1,26 @@
-from odoo import models,api,fields
+from odoo import models, api, fields
 
 class Movie(models.Model):
-    _name = "mms.movie"    
-    _inherit = 'mail.thread'
-    _description = "Movie List"
+    _name = "mms.movie"  # Define the model name for movies
+    _inherit = 'mail.thread'  # Inherit mail thread functionality
+    _description = "Movie List"  # Description for the model
     
-    name = fields.Char("Movie Name", tracking=True)
-    synopsis = fields.Text("Synopsis", tracking=True)
-    actor_ids = fields.Many2many("mms.actor",string="Actors", tracking=True)
-    genre_ids = fields.Many2many("mms.genre",string="Genre", tracking=True)
-    contact_id = fields.Many2one("res.partner",string="Contact", tracking=True)
-    #message_ids = fields.One2many('mail.message', 'res_id', domain=[('model', '=', 'mms.movie')], string='Messages')
+    name = fields.Char("Movie Name", tracking=True)  # Field for movie name with tracking enabled
+    synopsis = fields.Text("Synopsis", tracking=True)  # Field for movie synopsis with tracking enabled
+    actor_ids = fields.Many2many("mms.actor", string="Actors", tracking=True)  # Many-to-many relationship with actors
+    genre_ids = fields.Many2many("mms.genre", string="Genre", tracking=True)  # Many-to-many relationship with genres
+    contact_id = fields.Many2one("res.partner", string="Contact", tracking=True)  # Many-to-one relationship with contacts
 
 class Actor(models.Model):
-    _name = "mms.actor"
-    _description = "Cast"
+    _name = "mms.actor"  # Define the model name for actors
+    _description = "Cast"  # Description for the model
     
-    name = fields.Char("Actor name")
-    movie_id = fields.Many2one("mms.movie",string="Movie")
-    
+    name = fields.Char("Actor name")  # Field for actor name
+    movie_id = fields.Many2one("mms.movie", string="Movie")  # Many-to-one relationship with movies
 
 class Genre(models.Model):
-    _name = "mms.genre"
-    _description = "Movie genre"
+    _name = "mms.genre"  # Define the model name for genres
+    _description = "Movie genre"  # Description for the model
     
-    name = fields.Char("Genre")
-    movie_id = fields.Many2one("mms.movie",string="Movie")
+    name = fields.Char("Genre")  # Field for genre name
+    movie_id = fields.Many2one("mms.movie", string="Movie")  # Many-to-one relationship with movies
