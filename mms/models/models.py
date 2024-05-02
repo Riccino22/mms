730a9a2413bdb6e191cf5694ce67c15ee42362e2
Riccino <riccardorinojosam@gmail.com>
2024-05-02 16:01:18 -0300

    created a tree view for movie

--- mms/models/models.py
+++ mms/models/models.py
@@ -10,6 +10,9 @@ class Movie(models.Model):
     actor_ids = fields.Many2many("mms.actor", string="Actors", tracking=True)  # Many-to-many relationship with actors
     genre_ids = fields.Many2many("mms.genre", string="Genre", tracking=True)  # Many-to-many relationship with genres
     contact_id = fields.Many2one("res.partner", string="Contact", tracking=True)  # Many-to-one relationship with contacts
+    format_id = fields.Many2one("mms.format", string="Format", tracking=True)  # Many-to-one relationship with formats
+    image = fields.Binary(string="Image", attachment=True, help="Select image here")  # Binary field for the movie image
+    trailer_url = fields.Char("Trailer URL", tracking=True)  # Field for the movie trailer URL
 
 class Actor(models.Model):
     _name = "mms.actor"  # Define the model name for actors
@@ -24,3 +27,9 @@ class Genre(models.Model):
     
     name = fields.Char("Genre")  # Field for genre name
     movie_id = fields.Many2one("mms.movie", string="Movie")  # Many-to-one relationship with movies
+
+class Format(models.Model):
+   _name = "mms.format"  # Specify the internal identifier for the model
+   _description = "Movie format"  # Provide a description for the model
+   name = fields.Char("Format")  # Define a field to store the format name
+  
