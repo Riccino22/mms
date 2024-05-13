defa8828eef0016182332ad91c300cafe763af1f
Riccino <riccardorinojosam@gmail.com>
2024-05-13 19:09:12 -0300

    created movie lend button

--- mms/__manifest__.py
+++ mms/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Movie Management System',
     'version': '1.0',
-    'depends': ['base','mail','crm'],
+    'depends': ['base','mail','account'],
     'author': "Ino",
     'category': 'Test',
     'description': """
@@ -11,16 +11,18 @@
     'data': [
         'security/ir.model.access.csv',
         'views/mms_movie.xml',
+        'views/mms_loan_request.xml',
         'views/mms_lend.xml',
         'views/mms_actor.xml',
         'views/mms_format.xml',
         'views/mms_genre.xml',
-        'views/mms_loan_request.xml',
-        #'views/res_partner.xml',
+        'views/res_partner.xml',
         'views/mms_menu.xml'
         #'wizard/mms_lend_movie.xml'
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
     ],
+    'installable': True,
+    'application': False
 }
--- mms/models/models.py
+++ mms/models/models.py
@@ -9,7 +9,7 @@ class Movie(models.Model):
     synopsis = fields.Text("Synopsis", tracking=True)  # Field for movie synopsis with tracking enabled
     actor_ids = fields.Many2many("mms.actor", string="Actors", tracking=True)  # Many-to-many relationship with actors
     genre_ids = fields.Many2many("mms.genre", string="Genre", tracking=True)  # Many-to-many relationship with genres
-    contact_id = fields.Many2one("res.partner", string="Contact", tracking=True)  # Many-to-one relationship with contacts
+    partner_id = fields.Many2one("res.partner", string="Contact", tracking=True)  # Many-to-one relationship with contacts
     format_id = fields.Many2one("mms.format", string="Format", tracking=True)  # Many-to-one relationship with formats
     image = fields.Binary(string="Image", attachment=True, help="Select image here")  # Binary field for the movie image
     trailer_url = fields.Char("Trailer URL", tracking=True)  # Field for the movie trailer URL
@@ -19,12 +19,24 @@ class Movie(models.Model):
         ( "loaned" , "loaned" )
     ])
     #lend_id = fields.Many2one("mms.lend", string="Lend")  
-    loan_request_ids = fields.One2many("mms.loan_request", "movie_id")
+    loan_request_ids = fields.One2many("mms.loan_request", "movie_id", string='Loan')
     
-class Contact(models.Model):
+    def lend_movie(self):
+        pass
+"""
+    loan_request_ids = fields.One2many("mms.loan_request", "movie_id", string='Loan', compute='_compute_loan')
+
+    @api.depends('loan_request_ids')
+    def _compute_loan(self):
+        for record in self:
+            record.loan_request_ids = self.env["mms.loan_request"].search([('movie_id', '=', record.name)])
+"""
+    
+class ResPartnerInherit(models.Model):
     _inherit = "res.partner"
     
-    #in_blacklist = fields.Boolean("In black list", default=False)
+    in_blacklist = fields.Boolean("In black list", default=False)
+    loan_request_ids = fields.One2many("mms.loan_request", "partner_id", string='Loan')
 
 class Actor(models.Model):
     _name = "mms.actor"  # Define the model name for actors
@@ -70,13 +82,22 @@ class LoanRequest(models.Model):
     _name = "mms.loan_request"
     _description = "Loan request"
     
-    movie_id = fields.Many2one("mms.movie", string="Movie")
+    movie_id = fields.Many2one("mms.movie", string="Movie", onchange="onchange_movie_id()")
+    
+    #movie_id = fields.Many2one("mms.movie", string="Movie", onchange="onchange_movie_id()")
     partner_id = fields.Many2one("res.partner", string="Contact")
     
     @api.depends("movie_id", "partner_id")
     def _compute_display_name(self):
         for record in self:
             record.display_name = "{movie} - {contact}".format(movie=record.movie_id.name, contact=record.partner_id.name)
+ 
+"""           
+    
+    @api.onchange('movie_id')
+    def onchange_movie_id(self):
+        if self.movie_id:
+            return {'domain': {'movie_id': [('name', '=', self.movie_id.name)]}}"""
 """
     @api.model
     def get_waiting_borrowers(self, movie_id):
